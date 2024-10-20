03985f552441f8c8be9095b2e262003885ac0114
Kenzo Cogswell <kcogswell@usf.edu>
2024-10-20 17:27:10 -0400

    Handle stock availability and disable purchase button when out of stock

--- main.js
+++ main.js
@@ -5,7 +5,33 @@ let sizeSelector = document.getElementById("size-selector");
 let priceElement = document.getElementById("product-price");
 
 // event listener for dynamic change
-sizeSelector.addEventListener("change", (event) => {
+sizeSelector.addEventListener("change", function(event) {
 let selectedPrice = event.target.value;
 priceElement.textContent = `$${selectedPrice}`;
 });
+
+// Task 3: Handle Stock Availability 
+let availability = document.getElementById("stock")
+let purchaseButton = document.getElementById("button")
+
+//initializing product array
+let stock = {
+    small: { price: 85, quantity: 43},
+    medium: { price: 110, quantity: 0}, // sold out
+    large: { price: 130, quantity: 19},
+    extralarge: { price: 160, quantity: 0}, // sold out
+}
+
+sizeSelector.addEventListener("change", function(event) {
+let selectedSize = event.target.value
+let selectedStock = stock[selectedSize] 
+priceElement.textContent = `$${selectedStock.price}`;
+
+if (selectedStock.quantity > 0) { // checking if product size is sold out or not 
+    availability.textContent = "In Stock"
+    purchaseButton.disabled = false;
+} else {
+    availability.textContent = "Out of Stock";
+    purchaseButton.disabled = true
+}
+}); // purchase button should be disabled for the medium and extra large sizes
